72547cb75bdc31f5211cca381d2dcfd572237f1b
k.maciejczuk <k.maciejczuk@samsung.com>
2017-02-02 12:20:55 +0100

    Reshapes pretty shapes

--- tbg.py
+++ tbg.py
@@ -6,26 +6,31 @@ class MyTurtles(turtle.Turtle):
         super(MyTurtles, self).__init__()
         #self.setup(800, 600)
         self.showturtle()
-	self.pretty_shape()
+	self.speed(0)
+	self.tracer(1, 0)
+	self.pretty_shape(length=100, min_angle=1, max_angle=90, min_steps=1, max_steps=5)
+
+    def get_random(self, x=0, y=100):
+        return random.randint(x, y)
 
     def genXY(self):
-        self.x = random.randint(1,180)
-        self.y = random.randint(1,180)
-        
-    def pretty_shape(self):
+        self.x = self.get_random(1, 180)
+        self.y = self.get_random(1, 180)
+
+    def pretty_shape(self, length=100, min_angle=1, max_angle=180, min_steps=2, max_steps=2):
         self.clear()
         self.reset()
-	self.genXY()
-        print str(self.x) + " " + str(self.y)
-        stop = False
-        while stop == False:
-            self.forward(100)
-            self.left(self.x)
-            self.forward(100)
-            self.left(self.y)
-            pos = self.position()
-            pos_cosm = (int(pos[0]), int(pos[1]))
-            if pos_cosm == (0, 0):
-                stop = True
+	jumps = [self.get_random(1, 90) for _ in xrange(self.get_random(min_steps, max_steps))]
+	print jumps
+	while True:
+	    for jump in jumps:
+		self.forward(length)
+		self.left(jump)
+	    pos = self.position()
+	    pos_cosm = (int(pos[0]), int(pos[1]))
+	    if pos_cosm == (0,0):
+		break 
+        
 
 tp = MyTurtles()
+raw_input("Done")
